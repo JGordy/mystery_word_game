0677f7d60bd39afde80a35c98e90c3951e1f6e32
Joseph Gordy <jgordy2424@gmail.com>
2017-08-19 12:11:29 -0400

    Guess counter working consistantly

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,15 @@ const router = express.Router();
 const fs = require("fs");
 
 const words = fs.readFileSync("/usr/share/dict/words", "utf-8").toLowerCase().split("\n");
-// let words = ["rebel", "chewbacca", "palindrome", "latte", "jalepenos"];
+
 let randomWord;
 let word = [];
-
+let remaining;
 
 router.get("/", function(req, res) {
   if (word == '') {
     randomWord = words[Math.floor(Math.random() * words.length)];
-    console.log(randomWord);
+    console.log("randomWord; ", randomWord);
 
     for (var i = 0; i < randomWord.length; i++) {
       let letters = {letter: randomWord[i],
@@ -19,7 +19,7 @@ router.get("/", function(req, res) {
                     placeholder: "_"}
       word.push(letters);
     }
-    let remaining =  {remainingGuesses: 8}
+    remaining =  {remainingGuesses: 8}
     word.push(remaining);
 
     res.render("game",{word: word});
@@ -31,17 +31,28 @@ router.get("/", function(req, res) {
 // also in the "if" correct guesses don't count against the player. Incorrect guesses should subtract from the total number of guesses
 router.post("/game", function(req, res) {
   let temp = req.body.letter;
+  // test
+  index = word.findIndex(x => x.remainingGuesses);
+
+  console.log(index);
+  //end test
   if (randomWord.includes(temp)) {
     word.forEach(function(single){
       if (single.letter == req.body.letter) {
         single.guessed = true;
           res.redirect("/");
+      } else {
+          // else statement
       }
     })
   } else {
+
+    word[index].remainingGuesses -= 1;
+
     let guess = {
       singleGuess: req.body.letter,
     }
+
     word.push(guess)
     console.log(word);
 
